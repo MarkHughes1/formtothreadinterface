61ca291a43b8088dbe31169bb4ba056bfd51ae33
MarkHughes1 <Mark.Hughes4096@gmail.com>
2015-08-02 17:25:59 +0100

    updates

---
+++ Formthreadinterface/Formthreadinterface/Worker.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Formthreadinterface
+{
+    public class Worker
+    {
+        /// <summary>
+        /// Does stuff
+        /// </summary>
+        /// <returns>true if stuff was done</returns>
+        public bool DoStuff()
+        {
+            var result = false;
+
+            int max = 1000000;
+            int n = 0;
+
+            for(n = 0; n < max; n++)
+            {
+
+            }
+
+
+            return result;
+        }
+    }
+}
